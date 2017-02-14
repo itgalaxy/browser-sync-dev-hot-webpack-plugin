f5fd0c04a77467d71c8fe4bb4a333a0fcb3a8489
evilebottnawi <alexander.krasnoyarov@itgalaxy.company>
2017-02-14 14:06:25 +0300

    Chore: initial public release.

--- README.md
+++ README.md
@@ -1,2 +1,66 @@
 # browser-sync-dev-hot-webpack-plugin
-Combines BrowserSync, webpack-dev-middleware, and webpack-hot-middleware into one ezpz plugin.
+
+[![NPM version](https://img.shields.io/npm/v/browser-sync-dev-hot-webpack-plugin.svg)](https://www.npmjs.org/package/browser-sync-dev-hot-webpack-plugin) 
+[![Travis Build Status](https://img.shields.io/travis/itgalaxy/browser-sync-dev-hot-webpack-plugin/master.svg?label=build)](https://travis-ci.org/itgalaxy/browser-sync-dev-hot-webpack-plugin) 
+[![dependencies Status](https://david-dm.org/itgalaxy/browser-sync-dev-hot-webpack-plugin/status.svg)](https://david-dm.org/itgalaxy/browser-sync-dev-hot-webpack-plugin) 
+[![devDependencies Status](https://david-dm.org/itgalaxy/browser-sync-dev-hot-webpack-plugin/dev-status.svg)](https://david-dm.org/itgalaxy/browser-sync-dev-hot-webpack-plugin?type=dev)
+
+Combines BrowserSync, webpack-dev-middleware, and webpack-hot-middleware into one plugin.
+
+## Install
+
+```shell
+npm install --save-dev browser-sync-dev-hot-webpack-plugin
+```
+
+## Usage
+
+```js
+const BROWSER_SYNC_OPTIONS = {};
+const DEV_MIDDLEWARE_OPTIONS = {
+    publicPath: '/my/public/path'
+};
+const HOT_MIDDLEWARE_OPTIONS = {};
+
+const webpackConfig = {
+    // Some options...
+    plugins: [
+        // Other plugins...
+        new BrowserSyncHotPlugin({
+            browserSyncOptions: BROWSER_SYNC_OPTIONS,
+            devMiddlewareOptions: DEV_MIDDLEWARE_OPTIONS,
+            hotMiddlewareOptions: HOT_MIDDLEWARE_OPTIONS,
+            callback() {
+                console.log('Callback')
+            }
+        })
+        // Other plugins...
+    ]
+    // Some options...
+};
+
+module.exports = webpackConfig;
+```
+
+## Options
+
+See related packages docs.
+
+## Related
+
+- [browser-sync](https://github.com/browsersync/browser-sync) - Keep multiple browsers & devices 
+in sync when building websites.
+
+- [webpack-dev-middleware](https://github.com/webpack/webpack-dev-middleware) - Offers a dev middleware for webpack, 
+which arguments a live bundle to a directory.
+
+- [webpack-hot-middleware](https://github.com/glenjamin/webpack-hot-middleware) - Webpack hot reloading 
+you can attach to your own server.
+
+## Contribution
+
+Feel free to push your code if you agree with publishing under the MIT license.
+
+## [Changelog](CHANGELOG.md)
+
+## [License](LICENSE)
---
+++ src/BrowserSyncDevHotWebpackPlugin.js
@@ -0,0 +1,99 @@
+'use strict';
+
+/* eslint-disable node/no-missing-require */
+const webpackDevMiddleware = require('webpack-dev-middleware');
+const webpackHotMiddleware = require('webpack-hot-middleware');
+const browserSync = require('browser-sync');
+/* eslint-enable node/no-missing-require */
+const merge = require('deepmerge');
+
+class BrowserSyncDevHotWebpackPlugin {
+    constructor(options) {
+        this.watcher = null;
+        this.compiler = null;
+        this.options = merge.all([
+            {
+                browserSyncOptions: {},
+                callback() {}, // eslint-disable-line no-empty-function
+                devMiddlewareOptions: {
+                    noInfo: true,
+                    stats: false
+                },
+                hotMiddlewareOptions: {}
+            },
+            options
+        ]);
+    }
+
+    apply(compiler) {
+        if (this.options.disable) {
+            return;
+        }
+
+        this.compiler = compiler;
+
+        compiler.plugin('done', () => {
+            if (!this.watcher) {
+                this.watcher = browserSync.create();
+                compiler.plugin('compilation', () => this.watcher.notify('Rebuilding...'));
+                this.start();
+            }
+        });
+    }
+
+    start() {
+        let browserSyncURLLocal = 'initialization';
+        let browserSyncURLExternal = 'initialization';
+        let browserSyncURLUI = 'initialization';
+        let browserSyncURLUIExternal = 'initialization';
+        const watcherConfig = merge.all([
+            {
+                proxy: {
+                    middleware: this.middleware(),
+                    proxyReq: [
+                        (proxyReq) => {
+                            proxyReq.setHeader('X-Browser-Sync-URL-Local', browserSyncURLLocal);
+                            proxyReq.setHeader('X-Browser-Sync-URL-External', browserSyncURLExternal);
+                            proxyReq.setHeader('X-Browser-Sync-URL-UI', browserSyncURLUI);
+                            proxyReq.setHeader('X-Browser-Sync-URL-UI-External', browserSyncURLUIExternal);
+                            proxyReq.setHeader('X-Dev-Middleware', 'On');
+                            proxyReq.setHeader('X-Hot-Middleware', 'On');
+                        }
+                    ]
+                }
+            },
+            this.options.browserSyncOptions
+        ]);
+
+        this.watcher.init(watcherConfig, (error, bs) => {
+            if (error) {
+                throw error;
+            }
+
+            const URLs = bs.getOption('urls');
+
+            browserSyncURLLocal = URLs.get('local');
+            browserSyncURLExternal = URLs.get('external');
+            browserSyncURLUI = URLs.get('ui');
+            browserSyncURLUIExternal = URLs.get('ui-external');
+
+            this.options.callback.bind(this);
+        });
+    }
+
+    middleware() {
+        const hotMiddlewareOptions = merge.all([
+            {
+                log: this.watcher.notify.bind(this.watcher)
+            },
+            this.options.hotMiddlewareOptions
+        ]);
+
+        this.webpackDevMiddleware = webpackDevMiddleware(this.compiler, this.options.devMiddlewareOptions);
+        this.webpackHotMiddleware = webpackHotMiddleware(this.compiler, hotMiddlewareOptions);
+
+        return [this.webpackDevMiddleware, this.webpackHotMiddleware];
+    }
+}
+
+module.exports = BrowserSyncDevHotWebpackPlugin;
